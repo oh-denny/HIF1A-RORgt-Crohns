823726979aa9e079c6ce58d08f7d6c013364e82c
denny <dennyleandro@gmail.com>
2026-06-25 09:12:40 -0300

    add

--- code/trajectory.r
+++ code/trajectory.r
@@ -61,6 +61,46 @@ table(
   pData(cds)$seurat_clusters
 )
 
+
+cols <- c(
+  "0" = "#A61C4B",
+  "1" = "#0F6C84",
+  "2" = "#2C6B6B",
+  "3" = "#AEB8B7",
+  "4" = "#6C7A3A",
+  "5" = "#D8C7AF",
+  "6" = "#162E93"
+)
+
+pData(cds)$seurat_clusters <- factor(
+  pData(cds)$seurat_clusters,
+  levels = names(cols)
+)
+
+p_cluster <- monocle::plot_cell_trajectory(
+  cds,
+  color_by = "seurat_clusters"
+) +
+  scale_color_manual(values = cols) +
+  theme_classic(base_size = 14) +
+  labs(color = "Cluster")
+
+p_cluster
+
+library(viridis)
+
+p_pseudotime <- monocle::plot_cell_trajectory(
+  cds,
+  color_by = "Pseudotime"
+) +
+  scale_color_viridis_c(option = "magma", direction = 1) +
+  theme_classic(base_size = 14) +
+  labs(color = "Pseudotime")
+
+p_pseudotime
+
+
+
 monocle::plot_cell_trajectory(cds, color_by = "seurat_clusters")
 monocle::plot_cell_trajectory(cds, color_by = "Pseudotime")
 monocle::plot_cell_trajectory(cds, color_by = "State")
