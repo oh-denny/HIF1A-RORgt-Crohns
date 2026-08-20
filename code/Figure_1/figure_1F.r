a95e9887626dadf5dc7342ee7e751540dca808d7
denny <dennyleandro@gmail.com>
2026-08-20 10:45:33 -0300

    update_1f

--- code/Figure_1/figure_1F.r
+++ code/Figure_1/figure_1F.r
@@ -8,8 +8,14 @@ ssgsea_results = run_pseudobulk_ssgsea_heatmap(
   min_cells = 9
 )
 
-tiff("figures/GSEA_1f.tiff", width = 9, height = 5, units = "in", res = 300)
+tiff("figures/GSEA_1f.tiff", width = 10, height = 7, units = "in", res = 300)
 ComplexHeatmap::draw(
-  ssgsea_results$heatmap
+  ssgsea_results$heatmap,
+  heatmap_legend_side = "bottom",
+  annotation_legend_side = "bottom",
+  padding = grid::unit(
+    c(2, 20, 2, 50),
+    "mm"
+  )
 )
 dev.off()
